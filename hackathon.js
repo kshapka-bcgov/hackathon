d8e09ba8d34b2885ff048b5bb7acfe7c3b4a7298
Michael Haswell <Michael.Haswell@gov.bc.ca>
2023-06-22 15:33:59 -0700

    Added score totals Added labels for inputs

--- hackathon.js
+++ hackathon.js
@@ -6,11 +6,16 @@ var activitiesCompleted = document.getElementById("activities-completed");
 
 $( document ).ready(function() {
     console.log( "ready!" );
+	let total = 0;
 
 	$("#submit-activity").on("click", (event) => {
 		const score = $("#activity-score").val();
 		const activity = $("#custom-activity").val();
 
 		$("#activities-completed").append("<li>" + activity + " - " + score + "</li>");
+		
+		total += parseInt(score);
+
+		$("#score-total").html("Total score: " + total);
 	});
 });
